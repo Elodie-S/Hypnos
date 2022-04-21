4ff0f203574520b88b99b78d438970fc4e750cd5
Elodie Salvi <elodie@salvi.fr>
2022-04-22 01:47:37 +0200

    erreur bdd

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php 
 
 // Connect to DB
-try {
-    $bdd = new PDO('mysql:host=clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=qsk9n8kar1sbyuhc;charset=utf8;', 'qt271pmrbtmyi652', 'kbjk4ssnkpp4w2a3');
-    } catch (Exception $e){die('Une erreur est survenue : '. $e->getMessage());}
+$conn = new mysqli("clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "qt271pmrbtmyi652", "kbjk4ssnkpp4w2a3", "qsk9n8kar1sbyuhc");
 
 require './libs/loginReq.php';
 require './libs/signupGuestReq.php';
