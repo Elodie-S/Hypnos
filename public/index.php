3c368d23902ec65a70874642c8c83fb0a4911c92
Elodie Salvi <elodie@salvi.fr>
2022-04-22 01:12:00 +0200

    Add Procfile

--- public/index.php
+++ public/index.php
@@ -1,19 +1,10 @@
 <?php 
 
-//Get Heroku connection information
-$db_server = "clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$db_username = "qt271pmrbtmyi652";
-$db_password = "kbjk4ssnkpp4w2a3";
-$db_db = "qsk9n8kar1sbyuhc";
-$active_group = 'default';
-$query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($db_server, $db_username, $db_password, $db_db);
+$conn = new mysqli("clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "qt271pmrbtmyi652", "kbjk4ssnkpp4w2a3", "qsk9n8kar1sbyuhc");
 
 require 'libs/loginReq.php';
 require 'libs/signupGuestReq.php';
-
-session_start();
 
 ?>
 
@@ -34,7 +25,7 @@ session_start();
 
 <body>
 
-<?php require_once 'includes/header.php'; ?>
+<?php require_once '../includes/header.php'; ?>
 
     <!------------------------- Photo d'accueil ------------------------->
     
@@ -176,7 +167,7 @@ session_start();
 
     <?php
     
-    require('libs/messaging.php');
+    require './libs/messaging.php';
     ?>
                     <h1 class="title">Contact</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quo, eos nostrum dolore nobis tempora laborum.</p>
